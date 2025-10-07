23cf10e86baa2b463b875929d03aa8d606a41c25
Gabriel Meneses <gabomendua@gmail.com>
2025-10-07 17:58:40 -0600

    Update Clase05-01-1.py

---
+++ semana-5/Clase05-01-1.py
@@ -0,0 +1,10 @@
+### Ejercicios sobre tuplas
+
+tupla1 = (1,12,255,1289,60,000) #al colocar parentesis se hacen las tuplas
+print(tupla1)
+print(len(tupla1))
+print(tupla1[3:])
+
+tupla1[-1] = 3 #este error no es un error, es una "excepción"
+
+print(tupla1) 
