9eaba7b317d167c5c59616a2bc8b91eadb7b476d
Gabriel Meneses <gabomendua@gmail.com>
2025-10-08 14:27:00 -0600

    Create ayudantia1.py

---
+++ Ayudantias/ayudantia1.py
@@ -0,0 +1,24 @@
+# num1 = int, input()
+# if num1 == int:
+# num2 = int, input()
+
+
+######################Ejercicio 2#########################################
+
+# a, b = map(int, input().split())
+# print(a+b)
+
+####################Ejercicio 3######################################
+
+num_manzana = int(input())
+cliente = 0
+while num_manzana != 1:
+    cliente_final=cliente+1
+    if num_manzana%2 == 0:
+        num_manzana = num_manzana/2
+        print(num_manzana)
+    else:
+        num_manzana = (num_manzana*3)+1
+        print(num_manzana)
+else:
+    print(cliente_final)
