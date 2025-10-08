d651be5b6118ee610906d447ae3de24e6b575eea
Gabriel Meneses <gabomendua@gmail.com>
2025-10-07 18:07:12 -0600

    Update Clase05-01-1.py

--- semana-5/Clase05-01-1.py
+++ semana-5/Clase05-01-1.py
@@ -1,10 +1,15 @@
 ### Ejercicios sobre tuplas
 
+# una tupla es inmutable
+# las listas son mutables
+
+
 tupla1 = (1,12,255,1289,60,000) #al colocar parentesis se hacen las tuplas
+lista1 = [1,12,255,1289,60,000] #los corchetes hacen una lista acá
 print(tupla1)
 print(len(tupla1))
 print(tupla1[3:])
+print(lista1)
+lista1[-1] = 3 # a las listas se le pueden hacer modificaciones y a las tuplas no
 
-tupla1[-1] = 3 #este error no es un error, es una "excepción"
-
-print(tupla1) 
+print(lista1) 
