5669d2144ac39534a378b28399ee0a188e67660a
Gabriel Meneses <gabomendua@gmail.com>
2025-10-09 16:35:35 -0600

    practica

--- Ayudantias/ayudantia1.py
+++ Ayudantias/ayudantia1.py
@@ -1,6 +1,10 @@
-# num1 = int, input()
-# if num1 == int:
-# num2 = int, input()
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+tupla = (num1, num2, num3, num4)
+nombre=("LEN", "IDN", "LCJ", "ISDN")
+print(nombre[tupla.index(max( ))])
 
 
 ######################Ejercicio 2#########################################
@@ -11,16 +15,16 @@
 ####################Ejercicio 3######################################
 #el while no necesita usar un "else:"
 
-num_manzana = int(input())
-cliente = 0
+# num_manzana = int(input())
+# cliente = 0
 
-while num_manzana != 1: 
-    cliente_final=cliente+1
-    if num_manzana%2 == 0:
-        num_manzana = num_manzana/2
-        print(num_manzana)
-    else:
-        num_manzana = (num_manzana*3)+1
-        print(num_manzana)
+# while num_manzana != 1: 
+#     cliente=cliente+1
+#     if num_manzana%2 == 0:
+#         num_manzana = num_manzana/2
+#         print(num_manzana)
+#     else:
+#         num_manzana = (num_manzana*3)+1
+#         print(num_manzana)
 
-print(cliente_final)
+# print(cliente)
