5bc4990db0d55e159792dd6cf2c9967aecfde231
Gabriel Meneses <gabomendua@gmail.com>
2025-10-09 18:06:23 -0600

    Create Clase05-2-Guia.py

---
+++ semana-5/Clase05-2-Guia.py
@@ -0,0 +1,63 @@
+"""Ejercicio 1
+
+enero=int(input("digite las cantidades de Enero: "))
+febrero=int(input("digite las cantidades de Febrero: "))
+marzo=int(input("digite las cantidades de Marzo: "))
+
+costos=(enero*1.25+febrero*1.38+marzo*1.14)
+#uso de los f-strings
+print(f"Las cantidades de enero, febrero y marzo son:" )
+print(f"{enero}, {febrero} y {marzo} con un costo")
+print(f"de ${costos:.2f}") #.2f es para colocar dos decimales
+"""
+
+"""Ejercicio 2, 3
+
+dias=["lunes", "martes", "miercoles", "jueves", "viernes"]
+
+lu= int(input("Lunes: "))
+dias[0] = lu
+ma= int(input("Martes: "))
+dias[1] = ma
+mi= int(input("Miercoles: "))
+dias[2] = mi
+ju= int(input("Jueves: "))
+dias[3] = ju
+vi= int(input("Viernes: "))
+dias[4] = vi
+print(dias)
+print(f"La produccion de la semana fue: {lu+ma+mi+ju+vi}")
+"""
+
+"""Ejercicio 4
+
+alumno = ("Diego", "Franquito", "Clavito", "Aby", "Alvin", "Medranito", "Genesis")
+
+nino= int(input("Ingrese el orden del niño que desea saber (1-7): "))
+
+print(f"El alumno que ingresó como número {nino} es {alumno[nino-1]}") #como mandar a llamar de una tupla
+"""
+
+"""Ejercicio 5
+
+nombre=input("Escribe tu nombre: ")
+apellido=input("Escribe tu apellido: ")
+
+print("Estas son las propuestas que se le brindan: ")
+print(f"propuesta 1: {nombre.lower()}.{apellido.lower()}@ISDN.com")
+print(f"propuesta 2: {nombre[0].lower()}.{apellido.lower()}@ISDN.com")
+"""
+
+"""#Ejercicio 6
+
+salario = input("Ingrese su salario: ")
+print(salario[0]=="$")
+print(salario.count("$")==1) #cuenta el numero de veces que se repite "$" en el string
+"""
+
+"""#Ejercicio 7
+
+palabra_encriptada="DFGUPCCBJKAJ"
+
+print(palabra_encriptada[0::3])
+"""
