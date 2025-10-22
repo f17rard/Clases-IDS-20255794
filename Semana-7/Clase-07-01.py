eed9082e9c98261b0672c8aecd3a3ff8b451609f
Gabriel Meneses <gabomendua@gmail.com>
2025-10-22 16:43:39 -0600

    Update Clase-07-01.py

---
+++ Semana-7/Clase-07-01.py
@@ -0,0 +1,8 @@
+# datos = [1,2,"tres", ["lunes", "martes", "miercoles"]]
+# print(datos[3][2][3])
+
+numeros = ["uno", "dos", "tres"]
+print (numeros)
+numeros = numeros + ["cuatro", "cinco", "seis"]
+print(numeros)
+print(len(numeros))
