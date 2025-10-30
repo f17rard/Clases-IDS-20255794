50ac28e5588a7b3aa0310509fe640a837b4bea9f
Gabriel Meneses <gabomendua@gmail.com>
2025-10-29 18:05:48 -0600

    Update clase-08-ejercicio3.py

--- semana-8/clase-08-ejercicio3.py
+++ semana-8/clase-08-ejercicio3.py
@@ -8,7 +8,7 @@ print("Bienvenido a nuestro sistema de control de Alumnos.")
 menu_activo = True
 
 while menu_activo:
-    opcion = input("Elija la opcion (1: Ingresa alumno, 2: Consultar, 3: Modificar, 5: Salir): ")
+    opcion = input("Elija la opcion (1: Ingresa alumno, 2: Consultar, 3: Modificar, 4: Borrar, 5: Salir): ")
     if opcion == "5":
         menu_activo = False
     elif opcion == "1":
@@ -20,5 +20,10 @@ while menu_activo:
         n = int(input("Ingrese la posicion del alumno a modificar (1-4): "))
         a = input("ingrese el nuevo nombre del alumno: ")
         lista_alumnos[n-1] = a
+    elif opcion == "4":
+        borrado = lista_alumnos.pop(int(input("ingrese el numero del alumno a popear (1-4): "))-1)
+        print(f"usted a popeado a {borrado}")
+    else:
+        print("La opcion elegida no es valida.")
 
 print("Gracias por utilizar nuestro sistema.") 
